366eac0e4a2faab94ea7ea644526dd645dcb2cd7
DESKTOP-F6IC33L\Pimmez <pimhoreman@gmail.com>
2019-09-27 16:58:26 +0200

    Initialize Commit with audio, 1 main scene, json TellTale story (proto) , INK ThirdpartyAsset, 2 scripts and more

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public List<AudioClip> audioClips = new List<AudioClip>();
+	private List<string> audioTags = new List<string>();
+	private AudioSource audioSource;
+
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	private void PlayThisClip(string _incomingClip)
+	{
+		foreach (AudioClip clip in audioClips)
+		{
+			if (clip.name == _incomingClip)
+			{
+				audioSource.PlayOneShot(clip);
+			}
+		}
+	}
+
+	private void OnEnable()
+	{
+		AccesInk.AudioTagEvent += PlayThisClip;
+	}
+
+	private void OnDisable()
+	{
+		AccesInk.AudioTagEvent -= PlayThisClip;
+	}
+}
